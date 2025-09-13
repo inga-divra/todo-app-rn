c475603a921f8c051d437e43c14fb6fc18e441cc
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-09-13 17:49:44 +0300

    feat(ui): add linear gradient background and apply home screen styles.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,25 @@
 import { createHomeStyles } from '@/assets/styles/home.styles';
 import useTheme from '@/hooks/useTheme';
-import { Text, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 const Index = () => {
   const { toggleDarkMode, colors } = useTheme();
 
   const homeStyles = createHomeStyles(colors);
   return (
-    <SafeAreaView style={homeStyles.safeArea}>
-      <Text>HI</Text>
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>toggle the mode</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homeStyles.container}
+    >
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Text>HI</Text>
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>toggle the mode</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 export default Index;
