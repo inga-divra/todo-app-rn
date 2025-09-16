1f31635a99daee4da2fc2079767317764cd515ec
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-09-16 18:40:09 +0300

    feat(ui): set up Settings screen structure and header.
    Added the initial Settings screen layout with state hooks, theme integration, and header UI.

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,38 @@
+import { createSettingsStyles } from '@/assets/styles/settings.style';
+import useTheme from '@/hooks/useTheme';
+import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useState } from 'react';
 import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const SettingScreen = () => {
+  const [isAutoSync, setIsAutoSync] = useState(true);
+  const [isNotificationEnabled, setIsNotificationEnabled] = useState(true);
+
+  const { colors, isDarkMode, toggleDarkMode } = useTheme();
+
+  const settingsStyles = createSettingsStyles(colors);
   return (
-    <View>
-      <Text>SettingScreen</Text>
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={settingsStyles.container}
+    >
+      <SafeAreaView style={settingsStyles.safeArea}>
+        {/* HEADER */}
+        <View style={settingsStyles.header}>
+          <View style={settingsStyles.titleContainer}>
+            <LinearGradient
+              colors={colors.gradients.primary}
+              style={settingsStyles.iconContainer}
+            >
+              <Ionicons name='settings' size={28} color='#ffffff' />
+            </LinearGradient>
+            <Text style={settingsStyles.title}>Settings</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
